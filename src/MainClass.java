104b40f64184dbf4d0ec10e86923834897e8de9e
Juliettka <juliasemenovav@gmail.com>
2021-02-16 15:28:32 +0200

    Lesson 0 Test 2

--- src/MainClass.java
+++ src/MainClass.java
@@ -3,4 +3,8 @@ public class MainClass {
     {
         return 14;
     }
+    private int class_number = 20;
+    public int getClassNumber(){
+        return class_number;
+    }
 }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -2,6 +2,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MainClassTest extends MainClass {
+
     @Test
     public void testGetLocalNumber() {
         int expected = 14;
@@ -9,6 +10,15 @@ public class MainClassTest extends MainClass {
         actual = this.getLocalNumber();
         if (expected!=actual) {
             Assert.fail("Expected number is not equal to 14");
+        }
+    }
+    @Test
+    public void testGetClassNumber(){
+        int expected_number = 45;
+        int actual_number;
+        actual_number = this.getClassNumber();
+        if (actual_number < expected_number) {
+            Assert.fail("getClassNumber returns number less than 45");
         }
     }
 }
